6fba9a88864384553cab393f758aedefdd7f90e7
Alex Strigelskiy <astrigelskiy@specificdx.com>
2020-08-28 14:52:14 -0700

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,193 @@
+﻿using Dynamsoft.TWAIN;
+using Dynamsoft.TWAIN.Enums;
+using Dynamsoft.TWAIN.Interface;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dynamsoft
+{
+    public partial class Form1 : Form, IAcquireCallback
+    {
+        private TwainManager twain = null;
+        private string m_StrProductKey = "t0068MgAAAGb6rOq4pq+p1k2IfqBH43pALaGqfZ04JvYdp8urfgJPLc2v7Faact3eo05bSn8Pdw8XCLr0DMGF439wA7lx3pE=";
+        public Form1()
+        {
+            InitializeComponent();
+            var productKey = m_StrProductKey + ";" + LicenseLoader.ReadLocalLicense();
+            twain = new TwainManager(productKey);
+        }
+
+        public bool IfGetImageInfo => true;
+
+        public bool IfGetExtImageInfo => true;
+
+        public void OnPostAllTransfers()
+        {
+
+        }
+
+        public bool OnPostTransfer(Bitmap bit, string info)
+        {
+            bit.Save("d:\\temp\\ScannedImage.jpg", ImageFormat.Jpeg);
+            return true;
+        }
+
+        public void OnPreAllTransfers()
+        {
+
+        }
+
+        public bool OnPreTransfer()
+        {
+            return true;
+        }
+
+        public void OnSourceUIClose()
+        {
+
+        }
+
+        public void OnTransferCancelled()
+        {
+
+        }
+
+        public void OnTransferError()
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            twain.SelectSource();
+            twain.CloseSource();
+            twain.OpenSource();
+
+            twain.LogLevel = 1;
+            var setup = SetCapability(twain, TWCapability.ICAP_UNITS, (double)TWICapUNits.TWUN_PIXELS);
+            setup = setup && SetCapability(twain, TWCapability.ICAP_BITDEPTH, 48);
+            setup = setup && SetCapability(twain, TWCapability.ICAP_XRESOLUTION, 1000);
+
+            if (setup)
+            {
+                twain.IfShowUI = false;
+                //twain.IfShowIndicator = false;
+                twain.AcquireImage(this as IAcquireCallback);
+            }
+            
+            twain.CloseSource();
+        }
+        //twain.Capability = TWAIN.Enums.TWCapability.ICAP_XRESOLUTION;
+        //var didWeGetCap = twain.CapGet();
+        //twain.CapType = TWCapType.TWON_RANGE;
+        //if (didWeGetCap)
+        //{
+        //    var capCurrentIndex = twain.CapCurrentIndex;
+        //    var currentValue = twain.GetCapItems(capCurrentIndex);
+        //    twain.SetCapItems(capCurrentIndex, 1000);
+        //    twain.CapSet();
+        //}
+
+
+        //twain.Capability = TWAIN.Enums.TWCapability.ICAP_YRESOLUTION;
+        //didWeGetCap = twain.CapGet();
+        //if (didWeGetCap)
+        //{
+        //    twain.CapType = TWCapType.TWON_RANGE;
+        //    var capCurrentIndex = twain.CapCurrentIndex;
+        //    var currentValue = twain.GetCapItems(capCurrentIndex);
+        //    twain.SetCapItems(capCurrentIndex, 1000);
+        //}
+
+        //twain.IfShowUI = false;
+        //twain.IfShowIndicator = false;
+        //twain.AcquireImage(this as IAcquireCallback);
+        //twain.Capability = TWAIN.Enums.TWCapability.ICAP_XRESOLUTION;
+        //twain.CapValue = 1000;
+        //twain.CapType = TWAIN.Enums.TWCapType.TWON_RANGE;
+        //twain.CapSet();
+
+        //twain.Capability = TWAIN.Enums.TWCapability.ICAP_YRESOLUTION;
+        //result = twain.CapGet();
+        //value = twain.CapValue;
+
+        //twain.Capability = TWAIN.Enums.TWCapability.ICAP_YRESOLUTION;
+        //twain.CapValue = 1000;
+        //twain.CapType = TWAIN.Enums.TWCapType.TWON_RANGE;
+        //twain.CapSet();
+        private bool SetCapability(TwainManager twain, TWCapability capability, double value)
+        {
+            var logInfoPrefix = $"{MethodInfo.GetCurrentMethod().Name}->";
+            Debug.WriteLine($"{logInfoPrefix} capability:{capability}, value:{value}");
+            twain.Capability = capability;
+            bool didWeGetCapability = twain.CapGet();
+            if (!didWeGetCapability)
+            {
+                Debug.WriteLine($"{logInfoPrefix}failed to get capability");
+                return false;
+            }
+
+            Debug.WriteLine($"{logInfoPrefix}capability retrieved, capType:{twain.CapType}");
+            switch (twain.CapType)
+            {
+                case TWCapType.TWON_RANGE:
+                    Debug.WriteLine($"{logInfoPrefix}twain.CapCurrentValue");
+                    twain.CapCurrentValue = value;
+                    break;
+                case TWCapType.TWON_ENUMERATION:
+                case TWCapType.TWON_ARRAY:
+                    Debug.WriteLine($"{logInfoPrefix}twain.SetCapItems->result:{twain.SetCapItems(twain.CapCurrentIndex, value)}");
+                    break;
+                default:
+                    break;
+            }
+            var setResult = twain.CapSet();
+            Debug.WriteLine($"{logInfoPrefix}Set result for {capability} : {setResult}");
+            return setResult;
+        }
+
+        private void GetCapability(TwainManager twain, TWCapability capability)
+        {
+            twain.Capability = capability;
+            bool result = twain.CapGet();
+            if (result)
+            {
+                var currentType = twain.CapType;
+                Debug.Write($"The current value for capability:{capability}, type {currentType}, TWQC_SET:{twain.CapIfSupported(TWQC.TWQC_SET)}, TWQC_RESET:{twain.CapIfSupported(TWQC.TWQC_RESET)}");
+                switch (currentType)
+                {
+                    case TWCapType.TWON_RANGE:
+                        Debug.Write($"CapMinValue {twain.CapMinValue}," +
+                            $"CapMaxValue:{twain.CapMaxValue}, CapStepSize:{twain.CapStepSize}, CapDefaultValue:{twain.CapDefaultValue}, CapCurrentValue:{twain.CapCurrentValue}");
+                        break;
+                    case TWCapType.TWON_ONEVALUE:
+                        Debug.WriteLine($"CapValue: {twain.CapValue}" +
+                            $"CapValue:{twain.CapValue}, CapValueString:{twain.CapValueString}");
+                        break;
+                    case TWCapType.TWON_ENUMERATION:
+                        Debug.WriteLine($"CapNumItems {twain.CapNumItems}," +
+                            $"GetCapItems:{twain.GetCapItems(twain.CapCurrentIndex)}, GetCapItemsString:{twain.GetCapItemsString(twain.CapCurrentIndex)}," +
+                            $"CapCurrentIndex:{twain.CapCurrentIndex}, CapDefaultIndex:{twain.CapDefaultIndex}");
+                        break;
+                    case TWCapType.TWON_ARRAY:
+                        Debug.WriteLine($"CapNumItems {twain.CapNumItems}," +
+                            $"GetCapItems:{twain.GetCapItems(twain.CapCurrentIndex)}, GetCapItemsString:{twain.GetCapItemsString(twain.CapCurrentIndex)}");
+                        break;
+                }
+                Debug.WriteLine("");
+            }
+        }
+    }
+
+    
+}
---
+++ LicenseLoader.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Dynamsoft
+{
+    public class LicenseLoader
+    {
+        [DllImport("kernel32.dll")]
+        static extern uint GetSystemDirectory([Out] StringBuilder lpBuffer, uint uSize);
+
+        [DllImport("kernel32.dll")]
+        static extern uint GetWindowsDirectory([Out] StringBuilder lpBuffer, uint uSize);
+
+        private static string GetLicenseFilePath()
+        {
+            StringBuilder sbsSystemDir = new StringBuilder(256);
+            uint iLength = 0;
+            iLength = GetSystemDirectory(sbsSystemDir, 256);
+            if (iLength == 0)
+                return null;
+            StringBuilder sbsWindowsDir = new StringBuilder(256);
+            iLength = GetWindowsDirectory(sbsWindowsDir, 256);
+
+            if (iLength == 0)
+                return null;
+            if (sbsWindowsDir.Length > 0)
+            {
+                if (IntPtr.Size == 8)
+                {
+                    sbsWindowsDir.Append("\\SysWOW64\\DynamsoftDotNetTwain.lic");
+                }
+                else
+                {
+                    sbsWindowsDir.Append("\\System32\\DynamsoftDotNetTwain.lic");
+                }
+
+            }
+            return sbsWindowsDir.ToString();
+        }
+
+        private static List<string> ReadSystemLocalLicense(string strlicensePath)
+        {
+            List<string> liststringTempListLicense = null;
+            try
+            {
+                if (File.Exists(strlicensePath))
+                {
+                    string[] stringarrTempAllLines = File.ReadAllLines(strlicensePath);
+                    foreach (string strTemp in stringarrTempAllLines)
+                    {
+                        int iTempIndex = strTemp.IndexOf("SerialNo");
+                        if (iTempIndex != -1)
+                        {
+                            int iIndex1 = strTemp.IndexOf("=");
+                            if (iIndex1 != -1)
+                            {
+                                if (liststringTempListLicense == null)
+                                    liststringTempListLicense = new List<string>();
+                                string strTempLicense = strTemp.Substring((iIndex1 + 1), (strTemp.Length - (iIndex1 + 1)));
+                                liststringTempListLicense.Add(strTempLicense);
+                            }
+                        }
+                    }
+                }
+            }
+            catch
+            { }
+            return liststringTempListLicense;
+        }
+
+
+
+
+
+        private static string GetCurrentPath()
+        {
+            string strCurrentPath = System.IO.Directory.GetCurrentDirectory();
+            string strCurrentPathtxt = strCurrentPath + "\\lic.txt";
+            return strCurrentPathtxt;
+        }
+        private static string ReadLicense(string strPath)
+        {
+            try
+            {
+                string strLicenseMessage = System.IO.File.ReadAllText(strPath);
+                int beginIndex = strLicenseMessage.IndexOf("=\"");
+                int endIndex = strLicenseMessage.IndexOf("\";");
+                string strLicense = null;
+                if (beginIndex != -1 && endIndex != -1)
+                {
+                    strLicense = strLicenseMessage.Substring(beginIndex + 2, endIndex - beginIndex - 2);
+                }
+                return strLicense;
+            }
+            catch (Exception)
+            {
+                string strLicense = null;
+                return strLicense;
+            }
+        }
+
+        private static string CombineAllLicense(string strCurrentPathLicense)
+        {
+            if (strCurrentPathLicense != null)
+            {
+                try
+                {
+                    string strTempLicenseFilePath = GetLicenseFilePath();
+                    List<string> tempListLicense = ReadSystemLocalLicense(strTempLicenseFilePath);
+
+
+                    StringBuilder objProductKeys = new StringBuilder();
+
+                    foreach (string temp in tempListLicense)
+                    {
+                        if (temp != null)
+                        {
+                            objProductKeys.Append(temp);
+                            objProductKeys.Append(";");
+                        }
+                    }
+
+                    string strTempProductKey = objProductKeys.ToString();
+                    strTempProductKey = strTempProductKey + strCurrentPathLicense + ";";
+                    return strTempProductKey;
+                }
+                catch (Exception)
+                {
+                    string strTempProductKey = strCurrentPathLicense + ";";
+                    return strTempProductKey;
+                }
+            }
+            else
+            {
+                try
+                {
+                    string steTempLicenseFilePath = GetLicenseFilePath();
+                    List<string> liststringTempListLicense = ReadSystemLocalLicense(steTempLicenseFilePath);
+
+
+                    StringBuilder objProductKeys = new StringBuilder();
+
+                    foreach (string temp in liststringTempListLicense)
+                    {
+                        if (temp != null)
+                        {
+                            objProductKeys.Append(temp);
+                            objProductKeys.Append(";");
+                        }
+                    }
+
+                    string strTempProductKey = objProductKeys.ToString();
+                    return strTempProductKey;
+                }
+                catch (Exception)
+                {
+                    return null;
+                }
+            }
+
+        }
+
+        public static string ReadLocalLicense()
+        {
+            string strGetCurrentPath = LicenseLoader.GetCurrentPath();
+            string strReadLicense = LicenseLoader.ReadLicense(strGetCurrentPath);
+            string strCombineAllLicense = LicenseLoader.CombineAllLicense(strReadLicense);
+            return strCombineAllLicense;
+        }
+
+
+
+    }
+
+}
